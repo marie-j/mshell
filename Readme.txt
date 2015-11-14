687cc63393f5e71bfba471a6f6fe41535d504d97
marie <marie.jones@etudiant.univ-lille1.fr>
2015-11-14 14:52:17 +0100

    pipe fonctionnel

--- Readme.txt
+++ Readme.txt
@@ -6,3 +6,7 @@ TP7 : mshell
 Rendu 1 : 
 	gestions des signaux ; modification de sighandlers.c 
 	Pas de problème à la compilation. 
+
+Rendu 2 : 
+	Nous avons complété cmd.c ; tous les do sont fonctionnels.
+	Implémentation du pipe à 2 commandes , fonctionnel également.
--- pipe.c
+++ pipe.c
@@ -3,44 +3,46 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "pipe.h"
 
 void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
     
-    int ito,fd[2],res,status;
+    int fd[2],res,status;
 
-    if (nbcmd >= 3) {
-    	unix_error("too many arguments");
+    if (verbose) {
+        printf("Entering pipe\n");
     }
 
-    for (ito = 1; ito < nbcmd; ito++)
-    {
-    	if (!strcmp(cmds[ito],"to"))
-    	{
-    		break;
-    	}
+    if (verbose) {
+        printf("Pipe creation\n");
+    }
+    res = pipe(fd);
+    if (res == -1) {
+        unix_error("Error in pipe creation");
     }
 
-    if (ito < nbcmd) {
-        unix_error("wrong input");
-    }
 
-    cmds[ito] = (char *) 0;
 
-    pipe(fd);
 
     switch(fork()) {
         case -1 : 
             unix_error("fork cmd 1 error");
 
         case 0 : 
-            close(fd[0]);
+            if (verbose) {
+                printf("First son process\n");
+            }
             dup2(fd[1],STDOUT_FILENO);
-            execvp(argv[1],argv +1);
+            close(fd[0]);
+            execvp(cmds[0][0],cmds[0]);
             printf("execvp error\n");
             exit(EXIT_FAILURE);
 
         default : 
+            if (verbose) {
+                printf("Father process\n");
+            }
         ;
     }
 
@@ -49,9 +51,12 @@ void do_pipe(char *cmds[MAXCMDS][MAXARGS], int nbcmd, int bg) {
             unix_error("fork cmd 2 error");
 
         case 0 : 
+            if (verbose) {
+                printf("Second son process\n");
+            }
             close(fd[1]);
             dup2(fd[0],STDIN_FILENO);
-            execvp(argv[ito +1],argv +ito +1);
+            execvp(cmds[1][0],cmds[1]);
             printf("execvp 2 error\n");
             exit(EXIT_FAILURE);
 
